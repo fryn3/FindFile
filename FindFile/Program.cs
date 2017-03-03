788a445716d5cabb23d08347d1407b61026417c2
fryn3 <fryn3@mail.ru>
2017-03-03 20:10:31 +0300

    Версия 0.1... Нет обработчика исключений. Некорректно(либо я не знаю как) работает "?"

---
+++ FindFile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FindFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введи что ты ищешь?(text.txt, *.zip, ?.???)");
+			string file = Console.ReadLine();
+			Console.WriteLine(@"Введи где искать?(C:\Users\q\Desktop\book\)");
+			string address = Console.ReadLine();
+			uint i = 0;
+			Console.WriteLine("Результаты:");
+			foreach (FileInfo f in FindFile(address, file))
+			{
+				Console.Write(i++.ToString() + ".\t");
+				Console.WriteLine(f.FullName);
+			}
+			if (i == 0)
+				Console.WriteLine("Файлы не найдены!");
+
+		}
+		static FileInfo[] FindFile(string address, string filename)
+		{
+			DirectoryInfo dir = new DirectoryInfo(address);
+			FileInfo[] file_inf = null;
+			if (dir.Exists)
+			{
+				file_inf = dir.GetFiles(filename);
+
+				foreach (DirectoryInfo subdir in dir.GetDirectories())
+					__FindFile(subdir, filename, ref file_inf);
+			}
+			return file_inf;
+		}
+		static void __FindFile(DirectoryInfo dir, string filename, ref FileInfo[] file_inf)
+		{
+			if (dir.Exists)
+			{
+				FileInfo[] file_inf2 = dir.GetFiles(filename);
+				file_inf = file_inf.Concat(file_inf2).ToArray();
+
+				foreach (DirectoryInfo subdir in dir.GetDirectories())
+					__FindFile(subdir, filename, ref file_inf);
+			}
+		}
+	}
+}
